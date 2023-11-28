76ef2d2622f273c8b6d6a41fe8a7937eabc1c60a
Mizanur Rahman <mizan@arogga.net>
2023-11-28 17:43:05 +0600

    initial commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <View style={styles.header}>
+        <Text style={styles.headerText}>Facebook WebView</Text>
+      </View> */}
+      <WebView
+        source={{ uri: 'http://192.168.0.245:3000/' }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#4267B2', // Facebook blue color
+    padding: 10,
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App;
